27068e242f09720db26e50493fc1d96119e42ed4
SlavaPykhydko <slava17puh92@gmail.com>
2026-01-30 15:44:30 +0200

    Solution #2 only left

--- lesson_06/homework_6_1.py
+++ lesson_06/homework_6_1.py
@@ -3,17 +3,17 @@ str = input("Enter some sentence: ")
 list_1 = list(str)
 
 # Solution #1
-unique_chars_part_1 = [i for i in list_1 if list_1.count(i) == 1]
-unique_chars_part_2 = [i for i in list_1 if list_1.count(i) > 1]
-set_from_unique_chars_part_2 = set(unique_chars_part_2)
-list_2_from_set = list(set_from_unique_chars_part_2)
-print(f"unique_chars_part_1 = {unique_chars_part_1}")
-print(f"unique_chars_part_2 = {unique_chars_part_2}")
-print(f"list_2_from_set = {list_2_from_set}")
-unique_chars_part_1.extend(list_2_from_set)
-print(f"unique_chars_part_1 = {unique_chars_part_1}")
-count = len(unique_chars_part_1)
-print(f"count = {count}")
+# unique_chars_part_1 = [i for i in list_1 if list_1.count(i) >= 1]
+# unique_chars_part_2 = [i for i in list_1 if list_1.count(i) > 1]
+# set_from_unique_chars_part_2 = set(unique_chars_part_2)
+# list_2_from_set = list(set_from_unique_chars_part_2)
+# print(f"unique_chars_part_1 = {unique_chars_part_1}")
+# print(f"unique_chars_part_2 = {unique_chars_part_2}")
+# print(f"list_2_from_set = {list_2_from_set}")
+# unique_chars_part_1.extend(list_2_from_set)
+# print(f"unique_chars_part_1 = {unique_chars_part_1}")
+# count = len(unique_chars_part_1)
+# print(f"count = {count}")
 
 #Solution #2
 set_1 = set(list_1)
