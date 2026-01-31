82252e68f50ed851f996b7ec19eeae8af703b4aa
SlavaPykhydko <slava17puh92@gmail.com>
2026-01-31 19:40:37 +0200

    Added some manupulations with dict part #1

--- lesson_05/lesson_05.py
+++ lesson_05/lesson_05.py
@@ -1,4 +1,6 @@
-list_1 = [1, -2, 3, -5, 6]
+from operator import indexOf
+
+list_1 = [1, -2, 3, -5, 6, 8, 9 , 23]
 
 list_2 = [ l * -1 for l in list_1]
 print(list_2)
@@ -16,3 +18,20 @@ list_5 = list()
 list_5.append(len(list_4))
 list_5.append(sum)
 print(list_5)
+
+list_6 = [l for l in list_1 if list_1.index(l) %2 == 0]
+print(f" list_6 = {list_6}")
+
+dict_1 = {"a": 1, "b": 2, "c": 3}
+dict_2 = {"d": 4, "e": 5, "f": 6}
+# dict_3 = {**dict_1, **dict_2}
+dict_3 = {}
+dict_3.update(dict_1)
+dict_3.update(dict_2)
+print(dict_3)
+
+dict_4 = {"a": 1, "b": 2, "c": 3}
+dict_5 = {"d": 4, "e": 5, "f": 6}
+# dict_4 = dict_4 | dict_5
+dict_4 |= dict_5
+print(f"{dict_4= }")
