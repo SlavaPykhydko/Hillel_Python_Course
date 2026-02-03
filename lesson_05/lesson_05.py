79b2795b80ae01db74d8b6cdf97438cb689f7993
SlavaPykhydko <slava17puh92@gmail.com>
2026-02-03 20:00:39 +0200

    Added solutions for tasks with dict, tuple, list

--- lesson_05/lesson_05.py
+++ lesson_05/lesson_05.py
@@ -76,10 +76,65 @@ sorted_dict_9 = dict(sorted(dict_9.items(), key=lambda item: item[0]))
 print(sorted_dict_9)
 
 
-dict_10 = {'a': 1, 'b': 20, 'c': 3, 'd': 5, 'e': 134, 'k': 40, 'm': 8}
+dict_10 = {'a': 1, 'b': 20, 'c': 3, 'd': 5, 'e': 134, 'k': 40, 'm': -8}
 
 min2= min(dict_10.values())
 max2 = max(dict_10.values())
 print(type(dict_10.values()))
 print(min2)
 print(max2)
+
+min3 = max3 = i = 0
+for el in dict_10.values():
+    if i == 0:
+        min3 = el
+        i += 1
+        continue
+    if max3 < el:
+        max3 = el
+    if min3 > el:
+        min3 = el
+print(f'max3 = {max3}')
+print(f'min3 = {min3}')
+
+a = 1
+b = 2
+# temp = a
+# a = b
+# b = temp
+(a , b) = (b, a)
+print(f'{a=}, {b=}')
+
+set_1 = {1, 2, 3}
+set_1.update({'3434', '345', 'rdfv'})
+set_1.update(['dfd','mss'])
+print(set_1)
+
+a = 1
+b = 3
+c =4
+t1 = (a, b, c)
+l1 = [a, b, c]
+l1[0]=8
+print(t1)
+print(l1)
+
+set_3 = {'a', 'b', 'c', 'd', 'e'}
+set_4 = {'a', 'k', 'c', 'l', 'n'}
+set_res = set_3.intersection(set_4)
+print(f'set_res = {set_res}')
+set_res_2 = {el for el in set_3 if el in set_4}
+print(f'set_res_2 = {set_res_2}')
+
+# set_res_3 = set_3 - set_4
+# set_res_4 = set_4 - set_3
+
+set_res_3 = set_3.difference(set_4)
+set_res_4 = set_4.difference(set_3)
+print(f'set_res_3 = {set_res_3}')
+print(f'set_res_4 = {set_res_4}')
+
+l_2 = [1, 3, 4, 5, 5, 7, 2, 3, 4, 8]
+l_res = []
+[l_res.append(el) for el in l_2 if el not in l_res]
+print(f'l_res = {l_res}')
