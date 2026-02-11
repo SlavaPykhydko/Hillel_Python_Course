ecc94cf45730c8992d04a433ffa332a0a95a2d1b
SlavaPykhydko <slava17puh92@gmail.com>
2026-02-11 16:09:02 +0200

    Added __setattr__ function

--- lesson_09/lesson_09_1.py
+++ lesson_09/lesson_09_1.py
@@ -4,16 +4,24 @@ from ctypes.wintypes import PHKEY
 class Rhombus:
 
     def __init__(self, **kwargs):
-        if kwargs.get('side') > 0:
-            self.side = kwargs.get('side')
-        else:
-            raise ValueError("side cannot be less than 0")
+        self.side = kwargs.get('side')
         if (kwargs.get('tangle_a') + kwargs.get('tangle_b')) == 180:
             self.tangle_a = kwargs.get('tangle_a')
             self.tangle_b = kwargs.get('tangle_b')
         else:
             raise ValueError("sum a + b tangles must be equal to 180")
 
+    def __setattr__(self, side, value):
+        if value > 0:
+            super().__setattr__(side, value)
+        else:
+            raise ValueError("side cannot be less than 0")
+
+
+
 rhombus_1 = Rhombus(side=5, tangle_a=60, tangle_b=120)
+rhombus_1.__setattr__('side', 6)
+print(rhombus_1.__getattribute__('side'))
+
 
 
